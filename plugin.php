a092d7f0305801aaf3d4d96d90b84542f62154b1
Federico Guzman <federicoguzman@gmail.com>
2022-06-27 20:02:20 -0400

    v1.1.0
    fixed showAlert() message after save. language labels for forms save and cancel.

--- plugin.php
+++ plugin.php
@@ -18,19 +18,21 @@
         {
             global $layout;
             // $layout["title"] = "Hero Banner Plugin | Bludit";
-
-            // Check if the form was sent
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                // global $site;
-                // $site->set(array('title'=>$_POST['title']));
-                $this->setField('heroBgColor', Sanitize::html($_POST['heroBgColor']));
-                $this->setField('heroTitle', Sanitize::html($_POST['heroTitle']));
-                $this->setField('heroTitleColor', Sanitize::html($_POST['heroTitleColor']));
-                $this->setField('heroPhrase', Sanitize::html($_POST['heroPhrase']));
-                $this->setField('heroPhraseColor', Sanitize::html($_POST['heroPhraseColor']));
-                $this->setField('heroBtnLabel', Sanitize::html($_POST['heroBtnLabel']));
-                $this->setField('heroBtnLink', Sanitize::html($_POST['heroBtnLink']));
-                
+            try {
+                // Check if the form was sent
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    // global $site;
+                    // $site->set(array('title'=>$_POST['title']));
+                    $this->setField('heroBgColor', Sanitize::html($_POST['heroBgColor']));
+                    $this->setField('heroTitle', Sanitize::html($_POST['heroTitle']));
+                    $this->setField('heroTitleColor', Sanitize::html($_POST['heroTitleColor']));
+                    $this->setField('heroPhrase', Sanitize::html($_POST['heroPhrase']));
+                    $this->setField('heroPhraseColor', Sanitize::html($_POST['heroPhraseColor']));
+                    $this->setField('heroBtnLabel', Sanitize::html($_POST['heroBtnLabel']));
+                    $this->setField('heroBtnLink', Sanitize::html($_POST['heroBtnLink']));
+                }
+            } catch (\Throwable $th) {
+                //throw $th;
             }
         }
 
@@ -52,43 +54,51 @@
             $html .= $this->description();
             $html .= '</div>';
             $html .= '
-                <form method="post">
-                <input type="hidden" id="jstokenCSRF" name="tokenCSRF" value="'.$tokenCSRF.'">
-                <div class="form-group">
-                    <label for="heroBgColor">'.$L->get('hero-bg-color').'</label><br>
-                    <input type="color" id="heroBgColor" name="heroBgColor" value="'.$this->getValue('heroBgColor').'">
-                </div>
-                <hr>
-                <div class="form-group">
-                    <label for="heroTitle">'.$L->get('hero-title').'</label>
-                    <input type="text" class="form-control" id="heroTitle" name="heroTitle" value="'.$this->getValue('heroTitle').'">
-                </div>
-                <div class="form-group">
-                    <label for="heroTitleColor">'.$L->get('hero-title-color').'</label><br>
-                    <input type="color" id="heroTitleColor" name="heroTitleColor" value="'.$this->getValue('heroTitleColor').'">
-                </div>
-                <hr>
-                <div class="form-group">
-                    <label for="heroPhrase">'.$L->get('hero-phrase').'</label>
-                    <input type="text" class="form-control" id="heroPhrase" name="heroPhrase" value="'.$this->getValue('heroPhrase').'">
-                </div>
-                <div class="form-group">
-                    <label for="heroPhraseColor">'.$L->get('hero-phrase-color').'</label><br>
-                    <input type="color" id="heroPhraseColor" name="heroPhraseColor" value="'.$this->getValue('heroPhraseColor').'">
-                </div>
-                <hr>
-                <div class="form-group">
-                    <label for="heroBtnLabel">'.$L->get('hero-button-label').'</label>
-                    <input type="text" class="form-control" id="heroBtnLabel" name="heroBtnLabel" value="'.$this->getValue('heroBtnLabel').'">
-                </div>
-                <div class="form-group">
-                    <label for="heroBtnLink">'.$L->get('hero-button-link').'</label>
-                    <input type="text" class="form-control" id="heroBtnLink" name="heroBtnLink" value="'.$this->getValue('heroBtnLink').'">
-                </div>
-                <button type="submit" class="btn btn-primary">Save</button>
+                <form class="plugin-form" id="jsform" method="post" action="" autocomplete="off">
+                    <input type="hidden" id="jstokenCSRF" name="tokenCSRF" value="'.$tokenCSRF.'">
+                    <div class="form-group">
+                        <label for="heroBgColor">'.$L->get('hero-bg-color').'</label><br>
+                        <input type="color" id="heroBgColor" name="heroBgColor" value="'.$this->getValue('heroBgColor').'">
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        <label for="heroTitle">'.$L->get('hero-title').'</label>
+                        <input type="text" class="form-control" id="heroTitle" name="heroTitle" value="'.$this->getValue('heroTitle').'">
+                    </div>
+                    <div class="form-group">
+                        <label for="heroTitleColor">'.$L->get('hero-title-color').'</label><br>
+                        <input type="color" id="heroTitleColor" name="heroTitleColor" value="'.$this->getValue('heroTitleColor').'">
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        <label for="heroPhrase">'.$L->get('hero-phrase').'</label>
+                        <input type="text" class="form-control" id="heroPhrase" name="heroPhrase" value="'.$this->getValue('heroPhrase').'">
+                    </div>
+                    <div class="form-group">
+                        <label for="heroPhraseColor">'.$L->get('hero-phrase-color').'</label><br>
+                        <input type="color" id="heroPhraseColor" name="heroPhraseColor" value="'.$this->getValue('heroPhraseColor').'">
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        <label for="heroBtnLabel">'.$L->get('hero-button-label').'</label>
+                        <input type="text" class="form-control" id="heroBtnLabel" name="heroBtnLabel" value="'.$this->getValue('heroBtnLabel').'">
+                    </div>
+                    <div class="form-group">
+                        <label for="heroBtnLink">'.$L->get('hero-button-link').'</label>
+                        <input type="text" class="form-control" id="heroBtnLink" name="heroBtnLink" value="'.$this->getValue('heroBtnLink').'">
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="save">'.$L->get('hero-save-btn').'</button>
+                    <a class="btn btn-secondary" href="/admin/plugins" role="button">'.$L->get('hero-cancel-btn').'</a>
                 </form>
             ';
             return $html;
+        }
+
+        public function afterAdminLoad(){
+            global $L;
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                print('<script charset="utf-8">showAlert("'.$L->get('hero-save-msg').'");</script>');
+            }
         }
 
         public function adminSidebar()
